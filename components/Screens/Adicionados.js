d7f5d7e0fb8122310cfc611dc0322ee54b42042b
capsss <cavaposso@gmail.com>
2017-11-30 01:53:31 -0200

    lista

--- components/Screens/Adicionados.js
+++ components/Screens/Adicionados.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, FlatList, Image, ListView } from 'react-native';
 import firebase from 'firebase'
 import db from '../Main'
 
@@ -8,7 +8,7 @@ export default class Adicionado extends Component<{}> {
     data: []
   }
 
-  componentWillMount(){
+  lerbd = async () => {
     var database = firebase.database();
     email = firebase.auth().currentUser.email
     emailsplit = email.split(".")
@@ -17,48 +17,87 @@ export default class Adicionado extends Component<{}> {
       caminho += emailsplit[i]
     }
 
-    firebase.database().ref(caminho).on("value", snapshot => {
+    // this.dataRef = firebase.database().ref(caminho)
+    await firebase.database().ref(caminho).child('346364').on("value", snapshot => {
       this.setState({data: snapshot.val()})
+      // snapshot.forEach((idfilme) => {
+      //   idfilme.forEach((child) =>{
+      //     this.state.data.push({
+      //       nota_usuario: child.val(),
+      //       key: child.key
+      //     })
+      //   })
+        
+      // });
     })
-
   }
 
-  onpress(){
-    console.log(this.state.data)
-  }
+  // onpress(){
+  //   console.log(this.state.data)
+  // }
+  // exibir(){
+  //   <View style={styles.container}>
+  //       {console.log("geral: ", this.state.data)}
+  //       <Image
+  //           style = {styles.image}
+  //           source={{uri: `https://image.tmdb.org/t/p/w500${this.state.data.imageURL}`}}
+  //       />
+  //       <Text style={styles.titulo}>
+  //         {`${this.state.data.comentario_usuario}`}
+  //       </Text>
+  //       <Text style={styles.titulo}>
+  //         {`${this.state.data.nota_geral}`}
+  //       </Text>
+  //       <Text style={styles.titulo}>
+  //         {`${this.state.data.titulo}`}
+  //       </Text>
+  //   </View>
+  // }
 
   render() {
     return (
-      // <View style={styles.container}>
-      //   <FlatList
-      //       data={this.state.data}
-      //       keyExtractor={(x,i) => i}
-      //       renderItem={({ item }) => 
-      //           <View style={styles.container}>
-      //               <Image
-      //                   style = {styles.image}
-      //                   source={{uri: `https://image.tmdb.org/t/p/w500${item.imageURL}`}}
-      //               />
-      //               <Text style={styles.titulo}>
-      //                 {`${item.titulo}`}
-      //               </Text>
-      //               <Text style={styles.titulo}>
-      //                 {`${item.nota_usuario}`}
-      //               </Text>
-      //               <Text style={styles.titulo}>
-      //                 {`${item.comentario_usuario}`}
-      //               </Text>
-      //           </View>
-      //       }
+      <View style={styles.container}>
+        {console.log("geral: ", this.state.data)}
+        <Image
+            style = {styles.image}
+            source={{uri: `https://image.tmdb.org/t/p/w500${this.state.data.imageURL}`}}
+        />
+        <Text style={styles.titulo}>
+          {`${this.state.data.comentario_usuario}`}
+        </Text>
+        <Text style={styles.titulo}>
+          {`${this.state.data.nota_geral}`}
+        </Text>
+        <Text style={styles.titulo}>
+          {`${this.state.data.titulo}`}
+        </Text>
+      </View>
+
+      // <View>
+      //   <ListView
+      //     dataSource={this.state.dataSource}
+      //     renderRow={(rowData) => 
+      //       <Text>{rowData.nota_usuario}</Text>
+      //     }
       //   />
+      //   {this.onpress2}
       // </View>
 
-      <View>
-        <TouchableOpacity onPress={()  => this.onpress()}>
-          <Text> Lista de filmes adicionados </Text>
-        </TouchableOpacity>
-      </View>
+      // <View style={styles.container}>
+      //   {this.exibir()}
+      // </View>
+
+      // <View>
+      //   {this.onpress()}
+      //   <TouchableOpacity onPress={()  => this.onpress()}>
+      //     <Text> Lista de filmes adicionados </Text>
+      //   </TouchableOpacity>
+      // </View>
     );
+  }
+
+  componentWillMount = async () => {
+    this.lerbd()
   }
 
 }
